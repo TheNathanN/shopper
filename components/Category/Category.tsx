1d094df239c800eab5c187fa4c63fe713b64f4b8
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-08-29 22:05:45 -0700

    Fix threshold

--- components/Category/Category.tsx
+++ components/Category/Category.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { useInView } from 'react-hook-inview'
+import { useInView } from "react-hook-inview";
 import { AppContext } from "../../context/app-state";
 import { formatSlug } from "../../helpers/functions";
 import { Categories } from "../../helpers/types";
@@ -13,28 +13,26 @@ interface Props {
 const Category = ({ slug }: Props) => {
   const context = useContext(AppContext);
   const { data, setCurrentCategory, currentCategory } = context;
-  const [ref, isVisible] = useInView(
-    {
-      threshold: .5,
-    },
-  );
-  
+  const [ref, isVisible] = useInView({
+    threshold: 0.4,
+  });
+
   const filteredData: any = data?.data?.filter(
     (info: any): any => info.categories[0].slug === slug
   );
 
   useEffect(() => {
-    if (isVisible) setCurrentCategory(slug)
-  } ,[currentCategory, isVisible, slug, setCurrentCategory])
+    if (isVisible) setCurrentCategory(slug);
+  }, [currentCategory, isVisible, slug, setCurrentCategory]);
 
-  return (
-   filteredData ? 
-   <section className={styles.container} id={slug} ref={ref}>
+  return filteredData ? (
+    <section className={styles.container} id={slug} ref={ref}>
       <p className={styles["section-title"]}>{formatSlug(slug)}</p>
       {filteredData.map((info: any) => (
-          <ContentCard info={info} key={info.id} />
-        ))}
-    </section> : 
+        <ContentCard info={info} key={info.id} />
+      ))}
+    </section>
+  ) : (
     <div></div>
   );
 };
