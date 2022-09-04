04d5c617eb3cd207359ef2f4ae4b679b2920b36e
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-09-03 17:37:19 -0700

    Add Nav mobile functionality

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -4,11 +4,28 @@ import styles from "./Nav.module.scss";
 
 const Nav = () => {
   const context = useContext(AppContext);
-  const { user } = context;
+  const {
+    user,
+    setCurrentCategory,
+    setMobileMenu,
+    setShowSignin,
+    setShowCart,
+  } = context;
 
   return (
     <nav className={styles.container}>
-      <p className={styles.logo}>Audiophile</p>
+      <button
+        className={styles.btn}
+        onClick={() => {
+          window.scrollTo(0, 0);
+          setCurrentCategory("speakers");
+          setMobileMenu(false);
+          setShowSignin(false);
+          setShowCart(false);
+        }}
+      >
+        <p className={styles.logo}>Audiophile</p>
+      </button>
     </nav>
   );
 };
