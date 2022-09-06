20e191fa98e2995a8796c6c402f9dd844313642b
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-09-05 18:08:33 -0700

    Finish basic Popup styling

--- components/CartPopup/CartPopup.tsx
+++ components/CartPopup/CartPopup.tsx
@@ -1,10 +1,24 @@
-import React from "react";
 import styles from "./CartPopup.module.scss";
+import React, { useContext } from "react";
+import { AppContext } from "../../context/app-state";
 
 const CartPopup = () => {
+  const context = useContext(AppContext);
+  const { setCartPop, setShowCart, setSigninPop } = context;
   return (
     <div className={styles.container}>
-      <div>CartPopup</div>
+      <p className={styles.header}>
+        Added to Cart <i className="fa-solid fa-check"></i>
+      </p>
+      <button
+        onClick={() => {
+          setSigninPop(false);
+          setCartPop(false);
+          setShowCart(true);
+        }}
+      >
+        View Cart Here
+      </button>
     </div>
   );
 };
--- components/SigninPopup/SigninPopup.tsx
+++ components/SigninPopup/SigninPopup.tsx
@@ -1,10 +1,21 @@
 import styles from "./SigninPopup.module.scss";
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../../context/app-state";
 
 const SigninPopup = () => {
+  const context = useContext(AppContext);
+  const { user, setSigninPop, setShowCart } = context;
   return (
     <div className={styles.container}>
-      <div>SigninPopup</div>
+      <p className={styles.header}>Welcome {user?.name}!</p>
+      <button
+        onClick={() => {
+          setSigninPop(false);
+          setShowCart(true);
+        }}
+      >
+        View Your Cart Here
+      </button>
     </div>
   );
 };
